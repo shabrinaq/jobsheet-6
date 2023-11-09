ed079c3766b79b0bd151fa8004e98d92ad0acedf
shabrinaq <shabrinaqottrunnada@gmail.com>
2023-11-09 11:36:02 +0700

    first commit

---
+++ Selection2Exp124.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Selection2Exp124
+ */
+public class Selection2Exp124 {
+
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+
+        int year;
+
+        System.out.print("Input year = ");
+        year = input24.nextInt();
+
+        if (year%4 == 0) {
+            if (year%100 != 0)
+                System.out.println("Leap year");
+        } else {
+            System.out.println("Not a leap year");
+        }
+    }
+}
---
+++ Selection2Exp324.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/**
+ * Selection2Exp324
+ */
+public class Selection2Exp324 {
+
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+
+        String category;
+        int income, netSalary;
+        double tax=0;
+
+        System.out.print("Input category = ");
+        category = input24.nextLine();
+        System.out.print("Input income = ");
+        income = input24.nextInt();
+
+        if (category.equalsIgnoreCase("Worker")) {
+            if (income < 2000000) {
+                tax = 0.1;
+            } else if (income < 3000000) {
+                tax = 0.15;
+            } else {
+                tax = 0.2;
+            }
+            netSalary = (int) (income - (tax * income));
+            System.out.println("Net salary = " + netSalary);
+        } else if (category.equalsIgnoreCase("Businessman")) {
+            if (income < 2500000) {
+                tax = 0.15;
+            } else if (income < 3500000) {
+                tax = 0.2;
+            } else {
+                tax = 0.25;
+            }
+            netSalary = (int) (income - (tax * income));
+            System.out.println("Net salary = " + netSalary);
+        } else {
+            System.out.println("Innvalid category!");
+        }
+    }
+}
