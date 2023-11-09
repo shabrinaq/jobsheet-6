ee2e4704e593f3c21cc5edfc0888c993f81a30ef
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2023-11-09 22:13:19 +0700

    Add files via upload

---
+++ Selection2Exp224.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Selection2Exp224
+ */
+public class Selection2Exp224 {
+
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+
+       int angle1, angle2, angle3, totalAngel;
+       
+       System.out.print("Input angle 1 = ");
+       angle1 = input24.nextInt();
+       System.out.print("Input angle 2 = ");
+       angle2 = input24.nextInt();
+       System.out.print("Input angle 3 = ");
+       angle3 = input24.nextInt();
+
+        totalAngel = angle1+angle2+angle3;
+
+        if (totalAngel == 180) {
+            if ((angle1 == 90) || (angle2 == 90) || (angle3 == 90)) {
+                System.out.println("Right triangle");
+            } else if (angle1==angle2 && angle2==angle3 ) {
+                System.out.println("Equilateral triangle");
+            } else if (angle1==angle2 || angle2==angle3 || angle3==angle1) {
+                System.out.println("Isosceles triangle");
+            } else {
+                System.out.println("Not a right triangle");
+            }
+        } else {
+            System.out.println("Not a triangle");
+        }
+    }
+}
